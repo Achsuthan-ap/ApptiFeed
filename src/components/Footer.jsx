58ab4b6e79af189849b354e0eb8000f2c914f96e
Kathiresan0412 <thuraijathusan@gmail.com>
2023-12-06 13:22:28 +0530

    aa

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import Card from 'react-bootstrap/Card';
+export const FooterName = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 Dynamics Website. All rights reserved.</p>
+    </footer>
+  );
+};
+export function ImgOverlayExample() {
+    return (
+     "a"
+    );
+  }
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./index.css";
+import Card from 'react-bootstrap/Card';
+export const HeaderName = () => {
+  return (
+
+
+  <div className="headera">
+<img src="https://assets.ibcstack.com/web/bravo/it/img/logo.png" alt="" /></div>
+
+
+  );
+};
+export const NavName= () => {
+  return (
+    <nav>
+
+             <a class="mc-6" href="#">
+            Politics 
+            </a> 
+              <a class="mc-6" href="#">
+            Economy 
+            </a> 
+             <a class="mc-6" href="#">
+            Lifestyle 
+            </a> 
+              <a class="mc-7" href="#">
+            Astrology 
+            </a> 
+             <a class="mc-8" href="#">
+            Medicine 
+            </a> 
+             <a class="mc-9" href="#">
+            Sports
+            </a> 
+ 
+    </nav>
+  );
+};
+
